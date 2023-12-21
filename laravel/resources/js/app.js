a97e63e5aa74cc566ce60f98f7e6e970b820d860
Alexey <72097093+Aleksej-F@users.noreply.github.com>
2023-12-21 23:37:16 +0300

    vue connection

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import App from './src/App.vue'
+import { createApp } from 'vue'
+// import router from './router'
+
+
+const app = createApp(App)
+// app.use(VueMeta)
+// app.use(router)
+// app.use(store)
+app.mount('#app')
---
+++ laravel/resources/js/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  fghfghg
+  <!-- <component :is="layout" >
+    ghb
+    <RouterView />
+  </component> -->
+</template>
+<script>
+// import { RouterLink, RouterView } from 'vue-router'
+// import EmptyLayout from './layouts/EmptyLayout.vue'
+// import PageLayout from './layouts/PageLayout.vue'
+// import AdminLayout from './layouts/AdminLayout.vue'
+
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  computed:{
+    // layout(){
+    //   return (this.$route.meta.loyout || 'page') + '-layout'
+    // },
+    
+  },
+  components: {
+    // EmptyLayout, PageLayout, AdminLayout
+  },
+  created(){}
+} 
+</script>
